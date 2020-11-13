77931c395b102c21d928ad3217838e1693604ba7
status_coding <none>
2020-11-13 16:36:00 +0200

    Add solution for problem `A - Specific Tastes of Andre ` Link: https://codeforces.com/contest/1438/problem/A Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/1438/submission/98281141

---
+++ codeforces/1438/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int t,n,i;
+int main()
+{
+    cin>>t;
+    while(t)
+    {
+        t--;
+        cin>>n;
+        for(i=1;i<=n;i++)
+            cout<<"1 ";
+        cout<<'\n';
+    }
+    return 0;
+}
