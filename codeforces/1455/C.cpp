8de6b204cb8ff4e23031406a7f6f20c972a4cd28
status_coding <none>
2020-11-30 17:03:00 +0200

    Add solution for problem `C - Ping-pong` Link: https://codeforces.com/contest/1455/problem/C Tags: constructive algorithms, games, math, *1100 Ref: https://codeforces.com/contest/1455/submission/100011631

---
+++ codeforces/1455/C.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int t,a,b;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin>>t;
+    while(t)
+    {
+        t--;
+        cin>>a>>b;
+        cout<<a-1<<' '<<b<<'\n';
+    }
+    return 0;
+}
